526815d69376917d70847e3e4c8ba23a33d74a3d
Gwen <crushedsummers@gwens-air.lan>
2019-05-11 21:21:44 +0800

    project 4.1: chrome extension for school

---
+++ project4.1/project4.1Code/popup.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+	$("#HGC").on("click", function(){
+		chrome.tabs.create({"url": "https://classroom.google.com/u/0/c/MTUxMzc3MTI0MTFa"});
+	})
+	$("#HDrive").on("click", function(){
+		chrome.tabs.create({"url": "https://drive.google.com/drive/folders/1W1xKvQ9f0hunDKsext7w3IrN6IBaqZVW"});
+	})
+
+	$("#PGC").on("click", function(){
+		chrome.tabs.create({"url": "https://classroom.google.com/u/0/c/MzQ5NjYwOTk2NTFa"});
+	})
+	$("#PMB").on("click", function(){
+		chrome.tabs.create({"url": "https://vsa.managebac.com/student/classes/11018357/files"});
+	})
+	$("#PDrive").on("click", function(){
+		chrome.tabs.create({"url": "https://drive.google.com/drive/folders/0B1s8jXkDK92lfjNpSlM1cmUzc3F1cEdfb3M5TTExdTZsaEg3Wk0yTzVMNU1wcld4bmhXYlk"});
+	})
+	$("#PNotes").on("click", function(){
+		chrome.tabs.create({"url": "https://ibphysics.org/"});
+	})
+
+	$("#LF").on("click", function(){
+		chrome.tabs.create({"url": "https://progress.lawlessfrench.com/"});
+	})
+	$("#FDaily").on("click", function(){
+		chrome.tabs.create({"url": "https://docs.google.com/spreadsheets/d/1DUPMneG5rO8TEb62a4T03lYQcjzbfVpqVw1Uylo1120/edit#gid=0"});
+	})
+	$("#FGC").on("click", function(){
+		chrome.tabs.create({"url": "https://classroom.google.com/u/0/c/MTUxOTY3NzQwODZa"});
+	})
+	$("#FDrive").on("click", function(){
+		chrome.tabs.create({"url": "https://drive.google.com/drive/folders/1VlzuBwyKfDOiYWNCswL_fBTbdxGTq6ER"});
+	})
+	$("#FNotes").on("click", function(){
+		chrome.tabs.create({"url": "https://www.tremplinfle.com/site-map.html"});
+	})
+
+	$("#MGC").on("click", function(){
+		chrome.tabs.create({"url": "https://classroom.google.com/u/0/c/MTUxNzAzNjcyOTZa"});
+	})
+	$("#MK").on("click", function(){
+		chrome.tabs.create({"url": "https://vsa.kognity.com/study/app/mathematics-sl-new-version/"});
+	})
+
+	$("#calendar").on("click", function(){
+		chrome.tabs.create({"url": "https://portal.vsa.edu.hk/myportal/student/student_calendars.php"});
+	})
+	$("#timetable").on("click", function(){
+		chrome.tabs.create({"url": "https://cdn.discordapp.com/attachments/343209123028140032/576749388203032587/Screenshot_2019-05-11_at_8.35.49_PM.png"});
+	})
+	$("#gmail").on("click", function(){
+		chrome.tabs.create({"url": "https://mail.google.com/mail/u/0/?sw=2&token=%5B%22cftp%22,%22dd7be99629%22,%22Gsd-pu6HPyoyqnM3lvdrmA%5Cu003d%5Cu003d%22,%22ZyMrc6cBAw-rFZnvtXyj5A%5Cu003d%5Cu003d%22,%226765,6792,7164,7068,7395,7332,7403,7269,6804,7321,7419,7539,7292,7470,7363,7396,7096,7471,7236,7156,6949,7564,6984,7190,7327,6812,7460,7456,7464,7393,7196,7418,7461,7475,7361,7398,7469,6929,7529,6969,7445,7503,7549,7369,7027,7018,7527,7016,7427,6999,7310,7020,7530,7161,7437,7008,7200,7137,7509,7517,7523,7023,7502,7150,7487%22%5D&dilte=0&gme=0&sme=0#inbox"});
+	})
+	$("#HRM").on("click", function(){
+		chrome.tabs.create({"url": "https://classroom.google.com/u/0/c/NzEzMDAxOTMwMFpa"});
+	})
+	$("#toggl").on("click", function(){
+		chrome.tabs.create({"url": "https://toggl.com/app/timer"});
+	})
+	$("#UC").on("click", function(){
+		chrome.tabs.create({"url": "https://calendly.com/clee8"});
+	})
+});
+
+
+
+
+
+
